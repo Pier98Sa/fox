41efe137446259b1c7e3349bee315e84ed54c8c5
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2023-02-21 17:04:02 +0100

    creazione foxview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FoxviewComponent } from './foxview/foxview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoxviewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/foxview/foxview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foxview',
+  templateUrl: './foxview.component.html',
+  styleUrls: ['./foxview.component.css']
+})
+export class FoxviewComponent {
+
+}
